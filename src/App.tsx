3fdd3cee7adbfd7125a803e305d3d16d16e3dfac
Ronaldo Sic <ronysic13@gmail.com>
2024-10-16 23:45:32 -0600

    [INICIO] - Creacion del proyecto. Se esta configurado la aplicacion para hacer la landing page de ESCOD, en React 18 y con TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Home from "./pages/home.tsx";
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h3>ESCOD Our page</h3>
+    </div>
+  );
+};
+
+export default Home;
